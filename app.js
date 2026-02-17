3d3fbdd481b94d9cd900e2da3b5ef9ce65c52970
NVS-trupti <truptitambolkar@gmail.com>
2026-02-18 01:17:08 +0530

    fix cors for netlify

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 // Middleware
 // app.use(helmet()); // Commented out for now
 app.use(cors({
-  origin: "*",
+  origin: "https://magicweekend.netlify.app",
   credentials: true
 }));
 app.use(express.json());
